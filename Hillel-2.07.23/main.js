c7014a554169aa59c8eab20b81bc61bb5710459c
Lesia Humenna <lgumenna61@gmail.com>
2023-07-08 12:48:15 +0200

    addes JSON.stringify

--- Hillel-2.07.23/main.js
+++ Hillel-2.07.23/main.js
@@ -14,6 +14,20 @@ function generateList(array) {
 }
 const array = [1, 2, [1.1, 1.2, 1.3, 1.4], 3, 4, 5];
 
+const arraySrtings = JSON.stringify(array);
+console.log(arraySrtings);
+
 const listContainer = document.getElementById("list-container");
 const generatedList = generateList(array);
+listContainer.appendChild(generatedList);
+
+function checkProtocol(url) {
+  if (!/^https?:\/\//i.test(url)) {
+    url = "http://" + url;
+  }
+  return url;
+}
+
+let myUrl = "LesiaHumenna.com";
+myUrl = checkProtocol(myUrl);
 
